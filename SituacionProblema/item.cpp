3404d6641facb5be4425ca0727c3e2f9196de42d
MarioFriasPina <mario99fpina@gmail.com>
2021-11-26 14:29:59 -0700

    Added 8 new classes

--- SituacionProblema/item.cpp
+++ SituacionProblema/item.cpp
@@ -10,6 +10,26 @@ std::string Item::getName()
 	return pName;
 }
 
+std::string Item::getBrand()
+{
+	return pBrand;
+}
+
+int		Item::getQuantity()
+{
+	return pQuantity;
+}
+
+bool	Item::sellOne()
+{
+	if (pQuantity > 0)
+	{
+		pQuantity--;
+		return true;
+	}
+	return false;
+}
+
 Monitor::Monitor()
 {
 	pPrice = 0.0;
@@ -87,13 +107,178 @@ int CPU::getThreads()
 	return pThreads;
 }
 
+GPU::GPU(double price, std::string name, std::string brand, float speed, int ram, int bus)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pClockSpeed = speed;
+	pBusMemory = bus;
+	pGraphicRAM = ram;
+}
+
+float GPU::getClockSpeed()
+{
+	return pClockSpeed;
+}
+
+int GPU::getBusMemory()
+{
+	return pBusMemory;
+}
+
+int GPU::getGraphicRAM()
+{
+	return pGraphicRAM;
+}
+
+HDD::HDD(double price, std::string name, std::string brand, int size, int speed)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pCapacity = size;
+	pSpeed - speed;
+}
+
+int HDD::getSpeed()
+{
+	return pSpeed;
+}
+
+int HDD::getCapacity()
+{
+	return pCapacity;
+}
+
+SSD::SSD(double price, std::string name, std::string brand, int size, int speed)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pCapacity = size;
+	pSpeed - speed;
+}
+
+int SSD::getSpeed()
+{
+	return pSpeed;
+}
+
+int SSD::getCapacity()
+{
+	return pCapacity;
+}
+
+RAM::RAM(double price, std::string name, std::string brand, int size, int speed)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pSize = size;
+	pSpeed - speed;
+}
+
+int RAM::getSpeed()
+{
+	return pSpeed;
+}
+
+int RAM::getSize()
+{
+	return pSize;
+}
+
+Motherboard::Motherboard(double price, std::string name, std::string brand, int slots)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pSlots = slots;
+}
+
+int Motherboard::getSlots()
+{
+	return pSlots;
+}
+
+Power::Power(double price, std::string name, std::string brand, int power, int fan)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pWatts = power;
+	pFanSize = fan;
+}
+
+int		Power::getWatts()
+{
+	return pWatts;
+}
+
+int		Power::getFanSize()
+{
+	return pFanSize;
+}
+
+Keyboard::Keyboard(double price, std::string name, std::string brand, bool bluetooth, bool mechanical, int keys)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pBluetooth = bluetooth;
+	pKeys = keys;
+}
+
+int		Keyboard::getKeys()
+{
+	return pKeys;
+}
+
+bool	Keyboard::getBluetooth()
+{
+	return pBluetooth;
+}
+
+bool	Keyboard::getMechanical()
+{
+	return pMech;
+}
+
+Mouse::Mouse(double price, std::string name, std::string brand, bool bluetooth, int buttons)
+{
+	pPrice = price;
+	pName = name;
+	pBrand = brand;
+	pBluetooth = bluetooth;
+	pButtons = buttons;
+}
+
+int		Mouse::getButtons()
+{
+	return pButtons;
+}
+
+bool	Mouse::getBluetooth()
+{
+	return pBluetooth;
+}
+
 int main()
 {
-	Monitor lg(2399.99, "LG G1 65", "LG", 56, 32, 3840, 2160);
-	Monitor none;
+	Monitor	mon(2399.99, "LG G1 65", "LG", 56, 32, 3840, 2160);
+	CPU		cpu(1249.99, "Test", "ASUS", 3.1, 8, 12);
+	GPU		gpu(1999.99, "RTX 2070", "nvidia", 3.1, 16, 12);
+	HDD		mem(999.99, "Memoria", "samsung", 2048, 12);
+	SSD		solida(2000.0, "Solid", "crucial", 1024, 10);
+	RAM		ram(1000.0, "pirate", "logitech", 8, 12);
+	Motherboard	mb(899.99, "isf", "logitech", 4);
+	Power	pow(1200.0, "bfab", "fnsk", 311, 120);
+	Mouse	ms(300.0, "G305", "logitech", true, 5);
+	Keyboard	kb(700.0, "pricer", "razor", false, true, 104);
 
-	std::cout << lg.getName() << "\t" << lg.getPrice() << std::endl;
-	std::cout << none.getName() << "\t" << none.getPrice() << std::endl;
+	std::cout << mon.getName() << "\t" << mon.getPrice() << std::endl;
+	std::cout << cpu.getName() << "\t" << cpu.getPrice() << std::endl;
 
 	return 0;
 }
--- SituacionProblema/item.h
+++ SituacionProblema/item.h
@@ -6,12 +6,16 @@ class Item
 {
 protected:
 	double		pPrice;
+	int			pQuantity = 5;
 	std::string	pName;
 	std::string pBrand;
 
 public:
 	double		getPrice();
 	std::string	getName();
+	int			getQuantity();
+	std::string getBrand();
+	bool		sellOne();
 
 };
 
@@ -44,4 +48,110 @@ public:
 	float	getClockSpeed();
 	int		getCores();
 	int		getThreads();
+};
+
+class GPU : public Item
+{
+private:
+	int		pGraphicRAM;
+	int		pBusMemory;
+	float	pClockSpeed;
+
+public:
+	GPU();
+	GPU(double price, std::string name, std::string brand, float speed, int ram, int bus);
+	int		getGraphicRAM();
+	int		getBusMemory();
+	float	getClockSpeed();
+};
+
+class HDD : public Item
+{
+private:
+	int		pCapacity;
+	int		pSpeed; //RPM
+
+public:
+	HDD();
+	HDD(double price, std::string name, std::string brand, int size, int speed);
+	int		getCapacity();
+	int		getSpeed();
+};
+
+class SSD : public Item
+{
+private:
+	int		pCapacity;
+	int		pSpeed; //RPM
+
+public:
+	SSD();
+	SSD(double price, std::string name, std::string brand, int size, int speed);
+	int		getCapacity();
+	int		getSpeed();
+};
+
+class RAM : public Item
+{
+private:
+	int		pSize;
+	int		pSpeed;
+
+public:
+	RAM();
+	RAM(double price, std::string name, std::string brand, int size, int speed);
+	int		getSize();
+	int		getSpeed();
+};
+
+class Motherboard : public Item
+{
+private:
+	int		pSlots;
+
+public:
+	Motherboard();
+	Motherboard(double price, std::string name, std::string brand, int slots);
+	int		getSlots();
+};
+
+class Power : public Item
+{
+private:
+	int		pWatts;
+	int		pFanSize; //mm
+
+public:
+	Power();
+	Power(double price, std::string name, std::string brand, int watts, int fan);
+	int		getWatts();
+	int		getFanSize();
+};
+
+class Keyboard : public Item
+{
+private:
+	bool		pBluetooth;
+	bool		pMech;
+	int			pKeys; //114 / 104 / 87
+
+public:
+	Keyboard();
+	Keyboard(double price, std::string name, std::string brand, bool bluetooth, bool mechanical, int keys);
+	int		getKeys();
+	bool	getBluetooth();
+	bool	getMechanical();
+};
+
+class Mouse : public Item
+{
+private:
+	bool		pBluetooth;
+	int			pButtons;
+
+public:
+	Mouse();
+	Mouse(double price, std::string name, std::string brand, bool bluetooth, int buttons);
+	int		getButtons();
+	bool	getBluetooth();
 };
